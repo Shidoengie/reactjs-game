2cc838436d26afdb4da6c9846339343bfcb62334
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2025-02-26 22:39:51 +0000

    Added game lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import "./App.css";
+import { useState } from "react";
 
 function App() {
-	return <p>hello world</p>;
+	const [clicks, setClicks] = useState(0);
+	return (
+		<div className="w-full h-screen bg-gray-900 text-gray-100 flex items-center justify-center ">
+			<div className="flex gap-2">
+				<button
+					className="rounded-sm border border-transparent bg-blue-500 px-3 py-1 hover:bg-blue-400  active:hover:border-blue-200 "
+					onClick={() => {
+						setClicks(clicks + 1);
+					}}
+				>
+					click
+				</button>
+				<output className="rounded-sm px-3 py-1 bg-gray-500">
+					{clicks}
+				</output>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,3 @@
+/**
+ * This file is only here for vscode intelisense
+ */
